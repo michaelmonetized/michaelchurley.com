71a6bbdb47119e4d894070d7e31ced4cbfd41004
Michael Monetized <michaelmonetized@gmail.com>
2024-09-17 11:07:27 -0400

    more cleanup

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -3,20 +3,15 @@ import LogoSVG from "./logo-svg";
 
 export default function Logo({ multiplier = 1 }: { multiplier?: number }) {
   return (
-    <h1>
-      <Link
-        href="/"
-        title="Navigate to the homepage"
-        rel="home"
-        className="block"
-      >
-        <LogoSVG
-          alt={"Michael C. Hurley"}
-          title={"Hire Michael C. Hurley a professional business operator."}
-          width={(2802 / 25) * multiplier}
-          height={(1674 / 25) * multiplier}
-        />
-      </Link>
-    </h1>
+    <Link
+      href="/"
+      title="Navigate to the homepage"
+      rel="home"
+      className="block">
+      <LogoSVG
+        width={(2802 / 25) * multiplier}
+        height={(1674 / 25) * multiplier}
+      />
+    </Link>
   );
 }
